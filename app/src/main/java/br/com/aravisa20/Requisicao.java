4b79e54d3eb239c481097614fa02e6139f150540
Glauco <glaucogti@gmail.com>
2021-05-23 02:16:18 -0300

    Projeto Aravisa 2.0 update

--- app/src/main/java/br/com/aravisa20/Requisicao.java
+++ app/src/main/java/br/com/aravisa20/Requisicao.java
@@ -1,37 +1,60 @@
 package br.com.aravisa20;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+
 
 public class Requisicao extends AppCompatActivity {
 
-    String dados[] = { "Algoritmos", "Estrutura de Dados",
-            "Linguagens", "Interview Corner",
-            "GATE", "ISRO CS",
-            "UGC NET CS", "CS Subjects",
-            "Web Technologies" };
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requisicao);
+        Button btnTirarFoto = (Button) findViewById(R.id.btnTirarFoto);
+        btnTirarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tirarFoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(tirarFoto, REQUEST_IMAGE_CAPTURE);
+
+            }
+        });
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setTitle("ArAvisa");
-
-
-        ListView listrequisicao = (ListView) findViewById(R.id.lvrequisicao);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dados);
-        listrequisicao.setAdapter(adapter);
+        getSupportActionBar().setTitle("ArAvisa - Nova Requisição");
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView fotoTirada = (ImageView)findViewById(R.id.foto_registrada);
+            fotoTirada.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
--- app/src/main/java/br/com/aravisa20/ui/MinhasRequisicoes.java
+++ app/src/main/java/br/com/aravisa20/ui/MinhasRequisicoes.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.List;
+
 import br.com.aravisa20.R;
 
 /**
@@ -21,11 +23,9 @@ public class MinhasRequisicoes extends Fragment {
 
 
     String dados[]
-            = { "Algoritmos", "Estrutura de Dados",
-            "Linguagens", "Interview Corner",
-            "GATE", "ISRO CS",
-            "UGC NET CS", "CS Subjects",
-            "Web Technologies" };
+            = { "Rua com um grande buraco", "Falta de Iluminação",
+            "Vandalismo no Muro da Escola", "Recorrência de acidentes de transito",
+            "Muitos roubos no local a noite"};
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -72,7 +72,16 @@ public class MinhasRequisicoes extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_minhas_requisicoes, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_minhas_requisicoes, container, false);
+        ListView listView = (ListView) view.findViewById(R.id.lvminhasrequisicoes);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                dados
+        );
+        listView.setAdapter(listViewAdapter);
+        return view;
     }
 }
